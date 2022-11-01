bb000d4bae69ba495946df01d0eb8b26cd6d02a7
Konstantin <const@const.me>
2022-11-01 01:21:48 +0100

    DLL API, initial version

---
+++ MeshRepair/MeshRepair/API/interfaces.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "../../ComLightLib/comLightCommon.h"
+#include "Parameters.h"
+
+namespace MeshRepair
+{
+	struct DECLSPEC_NOVTABLE iSourceMesh : public ComLight::IUnknown
+	{
+		DEFINE_INTERFACE_ID( "{8cb3666b-83ac-4969-b0a2-6f628c7db797}" );
+	};
+
+	struct DECLSPEC_NOVTABLE iResultMesh : public ComLight::IUnknown
+	{
+		DEFINE_INTERFACE_ID( "{b78deaa8-d975-415c-8ec6-e075dfdb8fea}" );
+
+		virtual HRESULT COMLIGHTCALL getIndexBuffer(
+		  uint32_t& countTriangles, const uint32_t** rdi ) = 0;
+
+		virtual HRESULT COMLIGHTCALL getVertexBufferFP32(
+		  uint32_t& countVertices, const float** rdi ) = 0;
+	};
+
+	struct DECLSPEC_NOVTABLE iMeshRepair : public ComLight::IUnknown
+	{
+		DEFINE_INTERFACE_ID( "{f4ef4dfd-6016-45bb-ac93-2865c9c044e6}" );
+
+		virtual HRESULT COMLIGHTCALL createIndexedMeshFP32( uint32_t countVertices, const float* vb,
+		  uint32_t countTriangles, const uint32_t* ib, iSourceMesh** rdi ) = 0;
+
+		virtual HRESULT COMLIGHTCALL repair(
+		  iSourceMesh* mesh, const Parameters parameters, iResultMesh** rdi ) = 0;
+	};
+}  // namespace MeshRepair
---
+++ MeshRepair/MeshRepair/API/library.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "interfaces.h"
+#include "../../ComLightLib/comLightServer.h"
+
+namespace MeshRepair
+{
+	DLLEXPORT HRESULT COMLIGHTCALL createMeshRepair( iMeshRepair** rdi );
+}
