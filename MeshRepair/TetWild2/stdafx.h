6ab54bad11703d5e474f3f04e086e8e97e661c1c
Konstantin <const@const.me>
2022-11-02 13:50:33 +0100

    Copy-pasted sources into another project

--- MeshRepair/TetWild2/stdafx.h
+++ MeshRepair/TetWild2/stdafx.h
@@ -4,3 +4,10 @@
 #include <vector>
 #include <array>
 #include <algorithm>
+
+#include <string>
+#include <memory>
+#include <geogram/mesh/mesh_geometry.h>
+#include <geogram/basic/common.h>
+#include <geogram/mesh/mesh.h>
+#include <geogram/basic/geometry.h>
