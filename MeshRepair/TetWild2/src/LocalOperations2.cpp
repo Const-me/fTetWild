e3e4f230fc2c803d0e8cb1a4c6797676048fa30b
Konstantin <const@const.me>
2022-11-09 02:58:56 +0100

    Math optimizations

--- MeshRepair/TetWild2/src/LocalOperations2.cpp
+++ MeshRepair/TetWild2/src/LocalOperations2.cpp
@@ -11,42 +11,45 @@ namespace
 
 double floatTetWild::AMIPS_energy_aux_v2( const std::array<double, 12>& arr )
 {
-	const double helper_1 = arr[ 2 ];
-	const double helper_2 = arr[ 11 ];
-	const double helper_3 = arr[ 0 ];
-	const double helper_4 = arr[ 3 ];
-	const double helper_5 = arr[ 9 ];
-	const double helper_6 = 0.577350269189626 * helper_3 - 1.15470053837925 * helper_4 + 0.577350269189626 * helper_5;
-	const double helper_7 = arr[ 1 ];
-	const double helper_8 = arr[ 4 ];
-	const double helper_9 = arr[ 7 ];
-	const double helper_10 = arr[ 10 ];
-	const double helper_11 = 0.408248290463863 * helper_10 + 0.408248290463863 * helper_7 + 0.408248290463863 * helper_8 - 1.22474487139159 * helper_9;
-	const double helper_12 = 0.577350269189626 * helper_10 + 0.577350269189626 * helper_7 - 1.15470053837925 * helper_8;
-	const double helper_13 = arr[ 6 ];
-	const double helper_14 = -1.22474487139159 * helper_13 + 0.408248290463863 * helper_3 + 0.408248290463863 * helper_4 + 0.408248290463863 * helper_5;
-	const double helper_15 = arr[ 5 ];
-	const double helper_16 = arr[ 8 ];
+	const double a0 = arr[ 0 ];
+	const double a1 = arr[ 1 ];
+	const double a2 = arr[ 2 ];
+	const double a3 = arr[ 3 ];
+	const double a4 = arr[ 4 ];
+	const double a5 = arr[ 5 ];
+	const double a6 = arr[ 6 ];
+	const double a7 = arr[ 7 ];
+	const double a8 = arr[ 8 ];
+	const double a9 = arr[ 9 ];
+	const double a10 = arr[ 10 ];
+	const double a11 = arr[ 11 ];
 
-	const double helper_17 = 0.408248290463863 * helper_1 + 0.408248290463863 * helper_15 - 1.22474487139159 * helper_16 + 0.408248290463863 * helper_2;
-	const double helper_18 = 0.577350269189626 * helper_1 - 1.15470053837925 * helper_15 + 0.577350269189626 * helper_2;
-	const double helper_19 = 0.5 * helper_13 + 0.5 * helper_4;
-	const double helper_20 = 0.5 * helper_8 + 0.5 * helper_9;
-	const double helper_21 = 0.5 * helper_15 + 0.5 * helper_16;
-	const double helper_22 = ( helper_1 - helper_2 ) * ( helper_11 * helper_6 - helper_12 * helper_14 ) -
-					   ( -helper_10 + helper_7 ) * ( -helper_14 * helper_18 + helper_17 * helper_6 ) +
-					   ( helper_3 - helper_5 ) * ( -helper_11 * helper_18 + helper_12 * helper_17 );
+	const double helper_6 = 0.577350269189626 * a0 - 1.15470053837925 * a3 + 0.577350269189626 * a9;
+	const double helper_11 = 0.408248290463863 * a10 + 0.408248290463863 * a1 + 0.408248290463863 * a4 - 1.22474487139159 * a7;
+	const double helper_12 = 0.577350269189626 * a10 + 0.577350269189626 * a1 - 1.15470053837925 * a4;
+	const double helper_14 = -1.22474487139159 * a6 + 0.408248290463863 * a0 + 0.408248290463863 * a3 + 0.408248290463863 * a9;
+
+	const double helper_17 = 0.408248290463863 * a2 + 0.408248290463863 * a5 - 1.22474487139159 * a8 + 0.408248290463863 * a11;
+	const double helper_18 = 0.577350269189626 * a2 - 1.15470053837925 * a5 + 0.577350269189626 * a11;
+
+	const double helper_19 = 0.5 * a6 + 0.5 * a3;
+	const double helper_20 = 0.5 * a4 + 0.5 * a7;
+	const double helper_21 = 0.5 * a5 + 0.5 * a8;
+
+	const double tmpDiv = ( a2 - a11 ) * ( helper_11 * helper_6 - helper_12 * helper_14 ) -
+					   ( -a10 + a1 ) * ( -helper_14 * helper_18 + helper_17 * helper_6 ) +
+					   ( a0 - a9 ) * ( -helper_11 * helper_18 + helper_12 * helper_17 );
 
 	const double res =
-	  -( helper_1 * ( -1.5 * helper_1 + 0.5 * helper_2 + helper_21 ) + helper_10 * ( -1.5 * helper_10 + helper_20 + 0.5 * helper_7 ) +
-		 helper_13 * ( -1.5 * helper_13 + 0.5 * helper_3 + 0.5 * helper_4 + 0.5 * helper_5 ) +
-		 helper_15 * ( 0.5 * helper_1 - 1.5 * helper_15 + 0.5 * helper_16 + 0.5 * helper_2 ) +
-		 helper_16 * ( 0.5 * helper_1 + 0.5 * helper_15 - 1.5 * helper_16 + 0.5 * helper_2 ) + helper_2 * ( 0.5 * helper_1 - 1.5 * helper_2 + helper_21 ) +
-		 helper_3 * ( helper_19 - 1.5 * helper_3 + 0.5 * helper_5 ) + helper_4 * ( 0.5 * helper_13 + 0.5 * helper_3 - 1.5 * helper_4 + 0.5 * helper_5 ) +
-		 helper_5 * ( helper_19 + 0.5 * helper_3 - 1.5 * helper_5 ) + helper_7 * ( 0.5 * helper_10 + helper_20 - 1.5 * helper_7 ) +
-		 helper_8 * ( 0.5 * helper_10 + 0.5 * helper_7 - 1.5 * helper_8 + 0.5 * helper_9 ) +
-		 helper_9 * ( 0.5 * helper_10 + 0.5 * helper_7 + 0.5 * helper_8 - 1.5 * helper_9 ) ) /
-	  std::cbrt( helper_22 * helper_22 );
+	  -( a2 * ( -1.5 * a2 + 0.5 * a11 + helper_21 ) + a10 * ( -1.5 * a10 + helper_20 + 0.5 * a1 ) +
+		 a6 * ( -1.5 * a6 + 0.5 * a0 + 0.5 * a3 + 0.5 * a9 ) +
+		 a5 * ( 0.5 * a2 - 1.5 * a5 + 0.5 * a8 + 0.5 * a11 ) +
+		 a8 * ( 0.5 * a2 + 0.5 * a5 - 1.5 * a8 + 0.5 * a11 ) + a11 * ( 0.5 * a2 - 1.5 * a11 + helper_21 ) +
+		 a0 * ( helper_19 - 1.5 * a0 + 0.5 * a9 ) + a3 * ( 0.5 * a6 + 0.5 * a0 - 1.5 * a3 + 0.5 * a9 ) +
+		 a9 * ( helper_19 + 0.5 * a0 - 1.5 * a9 ) + a1 * ( 0.5 * a10 + helper_20 - 1.5 * a1 ) +
+		 a4 * ( 0.5 * a10 + 0.5 * a1 - 1.5 * a4 + 0.5 * a7 ) +
+		 a7 * ( 0.5 * a10 + 0.5 * a1 + 0.5 * a4 - 1.5 * a7 ) ) /
+	  std::cbrt( tmpDiv * tmpDiv );
 
 	return res;
 }
