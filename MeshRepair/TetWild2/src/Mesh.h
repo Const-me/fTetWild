fe9a092424cddb23101f529973c894e78669cd04
Konstantin <const@const.me>
2022-11-11 21:20:33 +0100

    Moved a class to another header

--- MeshRepair/TetWild2/src/Mesh.h
+++ MeshRepair/TetWild2/src/Mesh.h
@@ -10,35 +10,11 @@
 #include <unordered_set>
 #include <cassert>
 #include <queue>
-#include <random>
 #include "EdgesSet.h"
+#include "Random.hpp"
 
 namespace floatTetWild
 {
-
-	class Random
-	{
-	  public:
-		static int next( int min, int max )
-		{
-			static std::mt19937 gen( 42 );
-			int res = ( gen() - std::mt19937::min() ) / double( std::mt19937::max() - std::mt19937::min() ) * ( max - min ) + min;
-
-			return res;
-		}
-
-		template<typename T>
-		static void shuffle( std::vector<T>& vec )
-		{
-			for( int i = vec.size() - 1; i > 0; --i )
-			{
-				using std::swap;
-				const int index = next( 0, i + 1 );
-				swap( vec[ i ], vec[ index ] );
-			}
-		}
-	};
-
 	constexpr int8_t NOT_SURFACE = 127;
 	constexpr int8_t KNOWN_NOT_SURFACE = -63;
 	constexpr int8_t KNOWN_SURFACE = 63;
