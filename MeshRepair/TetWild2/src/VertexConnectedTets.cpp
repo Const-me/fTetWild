de4d15d968c49da91d225f0b3e557567b4a68255
Konstantin <const@const.me>
2022-11-12 18:04:01 +0100

    setIntersection, better memory management

--- MeshRepair/TetWild2/src/VertexConnectedTets.cpp
+++ MeshRepair/TetWild2/src/VertexConnectedTets.cpp
@@ -2,12 +2,46 @@
 #include "VertexConnectedTets.h"
 #include "LocalOperations.h"
 
+#define USE_ORIGINAL_VERSION 0
+
+namespace
+{
+	struct alignas( 64 ) IntersectionBuffers
+	{
+		std::vector<int> a, b, c;
+	};
+	thread_local IntersectionBuffers buffers;
+}  // namespace
+
 void floatTetWild::setIntersection( const VertexConnectedTets& a, const VertexConnectedTets& b, std::vector<int>& result )
 {
+#if USE_ORIGINAL_VERSION
 	set_intersection( a.vec, b.vec, result );
+#else
+	IntersectionBuffers& ib = buffers;
+	ib.a = a.vec;
+	ib.b = b.vec;
+	std::sort( ib.a.begin(), ib.a.end() );
+	std::sort( ib.b.begin(), ib.b.end() );
+	std::set_intersection( ib.a.begin(), ib.a.end(), ib.b.begin(), ib.b.end(), std::back_inserter( result ) );
+#endif
 }
 
 void floatTetWild::setIntersection( const VertexConnectedTets& a, const VertexConnectedTets& b, const VertexConnectedTets& c, std::vector<int>& result )
 {
+#if USE_ORIGINAL_VERSION
 	set_intersection( a.vec, b.vec, c.vec, result );
+#else
+	IntersectionBuffers& ib = buffers;
+	ib.a = a.vec;
+	ib.b = b.vec;
+	ib.c = c.vec;
+	std::sort( ib.a.begin(), ib.a.end() );
+	std::sort( ib.b.begin(), ib.b.end() );
+	std::sort( ib.c.begin(), ib.c.end() );
+
+	std::set_intersection( ib.a.begin(), ib.a.end(), ib.b.begin(), ib.b.end(), std::back_inserter( result ) );
+	auto it = std::set_intersection( result.begin(), result.end(), ib.c.begin(), ib.c.end(), result.begin() );
+	result.resize( it - result.begin() );
+#endif
 }
