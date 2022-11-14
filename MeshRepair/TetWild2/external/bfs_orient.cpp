bb0e9862bc7fb72cbf0659fca1f498b0ab2f946c
Konstantin <const@const.me>
2022-11-14 03:06:46 +0100

    Project settings, bfs_orient performance

--- MeshRepair/TetWild2/external/bfs_orient.cpp
+++ MeshRepair/TetWild2/external/bfs_orient.cpp
@@ -5,8 +5,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License
 // v. 2.0. If a copy of the MPL was not distributed with this file, You can
 // obtain one at http://mozilla.org/MPL/2.0/.
-
-#include "stdafx.h"
 #include "bfs_orient.h"
 #include <igl/orientable_patches.h>
 #include <Eigen/Sparse>
