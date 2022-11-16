2e42877cec9d4743a87ada5c4583edd2a7595fe0
Konstantin <const@const.me>
2022-11-16 18:32:04 +0100

    Refactor, cleanup unused

--- MeshRepair/TetWild2/src/VertexConnectedTets.cpp
+++ MeshRepair/TetWild2/src/VertexConnectedTets.cpp
@@ -19,8 +19,7 @@ void floatTetWild::setIntersection( const VertexConnectedTets& a, const VertexCo
 
 namespace
 {
-	// This scalar version is slightly faster. It's probably the latency of moving data to/from vectors.
-	static void merge3_scalar( const int* a1, const int* a2, const int* b1, const int* b2, const int* c1, const int* c2, std::vector<int>& result )
+	static void setIntersectionX3( const int* a1, const int* a2, const int* b1, const int* b2, const int* c1, const int* c2, std::vector<int>& result )
 	{
 		int a = *a1, b = *b1, c = *c1;
 		while( true )
@@ -60,77 +59,6 @@ namespace
 			c = *c1;
 		}
 	}
-
-	__forceinline __m128i load3( const int* x, const int* y, const int* z )
-	{
-		__m128i v = _mm_cvtsi32_si128( *x );
-		v = _mm_insert_epi32( v, *y, 1 );
-		v = _mm_insert_epi32( v, *z, 2 );
-		return v;
-	}
-
-	static void merge3_sse2( const int* a1, const int* a2, const int* b1, const int* b2, const int* c1, const int* c2, std::vector<int>& result )
-	{
-		while( true )
-		{
-			__m128i vec = load3( a1, b1, c1 );
-			while( true )
-			{
-				const __m128i perm_baa = _mm_shuffle_epi32( vec, _MM_SHUFFLE( 3, 0, 0, 1 ) );
-				const __m128i perm_ccb = _mm_shuffle_epi32( vec, _MM_SHUFFLE( 3, 1, 2, 2 ) );
-
-				const __m128i lt1 = _mm_cmplt_epi32( vec, perm_baa );	 // Compare for [ a, b, c ] < [ b, a, a ]
-				const __m128i lt2 = _mm_cmplt_epi32( vec, perm_ccb );	 // Compare for [ a, b, c ] < [ c, c, b ]
-				const __m128i lt = _mm_or_si128( lt1, lt2 );			 // Combine with bitwise OR
-				const uint32_t bmp = (uint32_t)_mm_movemask_epi8( lt );	 // Move bitmap to scalar register
-				if( 0 != ( bmp & 1 ) )
-				{
-					// a < b || a < c
-					a1++;
-					if( a1 >= a2 )
-						return;
-					vec = _mm_insert_epi32( vec, *a1, 0 );
-					continue;
-				}
-				if( 0 != ( bmp & 0x10 ) )
-				{
-					// b < a || b < c
-					b1++;
-					if( b1 >= b2 )
-						return;
-					vec = _mm_insert_epi32( vec, *b1, 1 );
-					continue;
-				}
-				if( 0 != ( bmp & 0x100 ) )
-				{
-					// c < a || c < b
-					c1++;
-					if( c1 >= c2 )
-						return;
-					vec = _mm_insert_epi32( vec, *c1, 2 );
-					continue;
-				}
-
-				// None of the above, we have the same integer in all 3 lanes of the vector
-				assert( _mm_cvtsi128_si32( vec ) == _mm_extract_epi32( vec, 1 ) );
-				assert( _mm_cvtsi128_si32( vec ) == _mm_extract_epi32( vec, 2 ) );
-
-				result.push_back( _mm_cvtsi128_si32( vec ) );
-				a1++;
-				b1++;
-				c1++;
-				if( a1 >= a2 || b1 >= b2 || c1 >= c2 )
-					return;
-				break;	// Break into the outer loop to reload the complete vector from these 3 source pointers
-			}
-		}
-	}
-
-	inline void merge3( const std::vector<int>& a, const std::vector<int>& b, const std::vector<int>& c, std::vector<int>& result )
-	{
-		merge3_scalar( a.data(), a.data() + a.size(), b.data(), b.data() + b.size(), c.data(), c.data() + c.size(), result );
-		// merge3_sse2( a.data(), a.data() + a.size(), b.data(), b.data() + b.size(), c.data(), c.data() + c.size(), result );
-	}
 }  // namespace
 
 void floatTetWild::setIntersection( const VertexConnectedTets& a, const VertexConnectedTets& b, const VertexConnectedTets& c, std::vector<int>& result )
@@ -144,7 +72,17 @@ void floatTetWild::setIntersection( const VertexConnectedTets& a, const VertexCo
 	a.ensureSorted();
 	b.ensureSorted();
 	c.ensureSorted();
-	merge3( a.vec, b.vec, c.vec, result );
+
+	const int* p1 = a.vec.data();
+	const int* p1End = p1 + a.vec.size();
+
+	const int* p2 = b.vec.data();
+	const int* p2End = p2 + b.vec.size();
+
+	const int* p3 = c.vec.data();
+	const int* p3End = p3 + c.vec.size();
+
+	setIntersectionX3( p1, p1End, p2, p2End, p3, p3End, result );
 
 #if COMPARE_VERSION
 	std::vector<int> orig;
