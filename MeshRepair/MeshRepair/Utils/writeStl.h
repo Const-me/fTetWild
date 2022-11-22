5cffa90acac582b719477ee419476f105c1971b2
Konstantin <const@const.me>
2022-11-22 04:03:20 +0100

    Cleanup unused

--- MeshRepair/MeshRepair/Utils/writeStl.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <vector>
-#include "../../TetWild2/src/Types.hpp"
-
-HRESULT writeStl( const std::vector<floatTetWild::Vector3> vertices, const std::vector<floatTetWild::Vector3i>& triangles, LPCTSTR path );
