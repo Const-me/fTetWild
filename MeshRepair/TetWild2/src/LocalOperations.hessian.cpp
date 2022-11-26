f972bfc8223a9f3b3b92d524d92ad566ac5db298
Konstantin <const@const.me>
2022-11-26 04:23:20 +0100

    Minor

--- MeshRepair/TetWild2/src/LocalOperations.hessian.cpp
+++ MeshRepair/TetWild2/src/LocalOperations.hessian.cpp
@@ -168,6 +168,11 @@ void floatTetWild::AMIPS_hessian_v2( const std::array<double, 12>& arr, Matrix3&
 	const double helper_101 = t07_x * t10_z;
 	const double helper_102 = helper_100 - helper_101 + cp_y;
 
+	const double t19_x = -cp_x - helper_61 + helper_63;
+	// cp = cp1 - cp2, but simplifying would break binary equality due to different summation order
+	const double t19_y = -helper_100 + helper_101 - cp1_y + cp2_y;
+	const double t19_z = -cp_z + t07_x * t10_y - t07_y * t10_x;
+
 	const __m256d t11 = add( v1, v2 );
 	STORE( t11 );
 
@@ -195,52 +200,49 @@ void floatTetWild::AMIPS_hessian_v2( const std::array<double, 12>& arr, Matrix3&
 	const double helper_85 = -st0;
 	const double helper_86 = 1.0 / helper_85;
 	const double helper_87 = helper_86 / cubicRoot( pow2( helper_85 ) );
-	const double helper_91 =
-	  0.666666666666667 * t08_x * t09_z + 0.666666666666667 * t07_z * t10_x - 0.666666666666667 * t09_x * t08_z - 0.666666666666667 * t07_x * t10_z;
 	const double helper_92 = -3.0 * v0_y + 1.0 * v1_y + 1.0 * v2_y + 1.0 * v3_y;
 
-	const double t16_x = v3_x - v0_x;
-	const double t16_y = v3_y - v0_y;
-	const double t16_z = v3_z - v0_z;
+	const __m256d t16 = sub( v3, v0 );
+	STORE( t16 );
 
-	const double t17_x = -t00_x + t02_x - t01_x;
-	const double t17_y = -t00_y + t02_y - t01_y;
-	const double t17_z = -t00_z + t02_z - t01_z;
+	const __m256d t17 = sub( sub( t02, t00 ), t01 );
+	STORE( t17 );
 
-	const double t18_x = -t03_x - t04_x + t06_x - t05_x;
-	const double t18_y = -t03_y - t04_y + t06_y - t05_y;
-	const double t18_z = -t03_z - t04_z + t06_z - t05_z;
+	const __m256d t18 = AvxMath::vectorNegate( add( sub( add( t03, t04 ), t06 ), t05 ) );
+	STORE( t18 );
 
-	const double helper_99 =
+	const double t20_x =
 	  -0.666666666666667 * t10_y * t16_z + 0.666666666666667 * t10_z * t16_y + 0.666666666666667 * t17_y * t18_z - 0.666666666666667 * t17_z * t18_y;
+	const double t20_y =
+	  0.666666666666667 * t08_x * t09_z + 0.666666666666667 * t07_z * t10_x - 0.666666666666667 * t09_x * t08_z - 0.666666666666667 * t07_x * t10_z;
+	const double t20_z =
+	  -0.666666666666667 * t08_x * t09_y + 0.666666666666667 * t08_y * t09_x + 0.666666666666667 * t07_x * t10_y - 0.666666666666667 * t07_y * t10_x;
+
 	const double helper_103 = -t10_y * t16_z + t10_z * t16_y + t17_y * t18_z - t17_z * t18_y;
-	const double helper_104 = 0.444444444444444 * helper_102 * helper_103 * product1 * helper_86 + helper_57 * helper_91 - helper_92 * helper_99;
+
+	const double helper_104 = 0.444444444444444 * helper_102 * helper_103 * product1 * helper_86 + helper_57 * t20_y - helper_92 * t20_x;
 	const double helper_105 = 1.85037170770859e-17 * v0_z * t12_z + 1.85037170770859e-17 * v0_y * t12_y + 1.85037170770859e-17 * v1_y * t15_y +
 							  1.85037170770859e-17 * v2_y * t14_y + 1.85037170770859e-17 * v3_y * t13_y + 1.85037170770859e-17 * v3_z * t13_z +
 							  1.85037170770859e-17 * v2_x * t14_x + 1.85037170770859e-17 * v1_z * t15_z + 1.85037170770859e-17 * v2_z * t14_z +
 							  1.85037170770859e-17 * v0_x * t12_x + 1.85037170770859e-17 * v1_x * t15_x + 1.85037170770859e-17 * t13_x * v3_x;
 
 	const double helper_106 = helper_64 * product1 * helper_86;
-	const double helper_107 =
-	  -0.666666666666667 * t08_x * t09_y + 0.666666666666667 * t08_y * t09_x + 0.666666666666667 * t07_x * t10_y - 0.666666666666667 * t07_y * t10_x;
-	const double helper_108 = -3.0 * v0_z + 1.0 * v3_z + 1.0 * v1_z + 1.0 * v2_z;
-	const double helper_109 = -cp_z + t07_x * t10_y - t07_y * t10_x;
-	const double helper_110 = 0.444444444444444 * helper_109 * product1 * helper_86;
-	const double helper_111 = helper_103 * helper_110 + helper_107 * helper_57 - helper_108 * helper_99;
+	const double helper_108 = -3.0 * v0_z + v3_z + v1_z + v2_z;
+	const double helper_110 = 0.444444444444444 * t19_z * product1 * helper_86;
+	const double helper_111 = helper_103 * helper_110 + t20_z * helper_57 - helper_108 * t20_x;
 	const double helper_116 = product1 * helper_86 * ( t16_x * t10_z - t10_x * t16_z + t17_x * t18_z - t18_x * t17_z );
-	const double helper_117 = -helper_100 + helper_101 - cp1_y + cp2_y;
-	const double helper_118 = -helper_102 * helper_110 + helper_107 * helper_92 + helper_108 * helper_91;
+	const double helper_118 = -helper_102 * helper_110 + t20_z * helper_92 + helper_108 * t20_y;
 	const double helper_119 = product1 * helper_86 * ( -t16_x * t10_y + t10_x * t16_y - t17_x * t18_y + t18_x * t17_y );
-	result_0( 0, 0 ) = st3 * ( helper_57 * helper_64 * st2 - pow2( helper_64 ) * helper_83 +
-							   0.666666666666667 * helper_64 * helper_84 * ( -cp_x - helper_61 + helper_63 ) + 3.0 );
-	result_0( 0, 1 ) = helper_87 * ( helper_104 - helper_105 * v1_z + helper_106 * helper_91 );
-	result_0( 0, 2 ) = helper_87 * ( helper_106 * helper_107 + helper_111 );
-	result_0( 1, 0 ) = helper_87 * ( helper_104 + helper_116 * helper_99 );
-	result_0( 1, 1 ) = st3 * ( -pow2( helper_117 ) * helper_83 + helper_117 * st2 * helper_92 + helper_117 * helper_84 * helper_91 + 3.0 );
-	result_0( 1, 2 ) = helper_87 * ( -helper_105 * v1_x - helper_107 * helper_116 + helper_118 );
-	result_0( 2, 0 ) = helper_87 * ( -helper_105 * v1_y + helper_111 + helper_119 * helper_99 );
-	result_0( 2, 1 ) = helper_87 * ( helper_118 - helper_119 * helper_91 );
-	result_0( 2, 2 ) = st3 * ( -helper_108 * helper_109 * st2 - 1.11111111111111 * pow2( helper_109 ) * helper_84 + 3.0 );
+
+	result_0( 0, 0 ) = st3 * ( helper_57 * helper_64 * st2 - pow2( helper_64 ) * helper_83 + 0.666666666666667 * helper_64 * helper_84 * t19_x + 3.0 );
+	result_0( 0, 1 ) = helper_87 * ( helper_104 - helper_105 * v1_z + helper_106 * t20_y );
+	result_0( 0, 2 ) = helper_87 * ( helper_106 * t20_z + helper_111 );
+	result_0( 1, 0 ) = helper_87 * ( helper_104 + helper_116 * t20_x );
+	result_0( 1, 1 ) = st3 * ( -pow2( t19_y ) * helper_83 + t19_y * st2 * helper_92 + t19_y * helper_84 * t20_y + 3.0 );
+	result_0( 1, 2 ) = helper_87 * ( -helper_105 * v1_x - t20_z * helper_116 + helper_118 );
+	result_0( 2, 0 ) = helper_87 * ( -helper_105 * v1_y + helper_111 + helper_119 * t20_x );
+	result_0( 2, 1 ) = helper_87 * ( helper_118 - helper_119 * t20_y );
+	result_0( 2, 2 ) = st3 * ( -helper_108 * t19_z * st2 - 1.11111111111111 * pow2( t19_z ) * helper_84 + 3.0 );
 }
 
 void floatTetWild::AMIPS_hessian( const std::array<Scalar, 12>& T, Matrix3& result_0 )
