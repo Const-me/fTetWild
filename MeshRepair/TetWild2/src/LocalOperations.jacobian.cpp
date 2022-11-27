613d9892efdf49322718590a6c9f4c80eedbc112
Konstantin <const@const.me>
2022-11-27 17:04:30 +0100

    Minor

--- MeshRepair/TetWild2/src/LocalOperations.jacobian.cpp
+++ MeshRepair/TetWild2/src/LocalOperations.jacobian.cpp
@@ -53,23 +53,23 @@ void floatTetWild::AMIPS_jacobian_v2( const std::array<Scalar, 12>& arr, Vector3
 	const double helper_23 = t2_z * t1_x - t1_z * t2_x;
 	const double helper_32 = t2_y * t1_x;
 	const double helper_37 = t2_x * t1_y;
-	const double helper_39 = helper_23 * t0_y - t0_z * ( helper_32 - helper_37 ) - t0_x * ( t2_z * t1_y - t1_z * t2_y );
-	const double helper_40 = 1.0 / cubicRoot( pow2( helper_39 ) );
+	const double s0 = helper_23 * t0_y - t0_z * ( helper_32 - helper_37 ) - t0_x * ( t2_z * t1_y - t1_z * t2_y );
+	const double s1 = 1.0 / cubicRoot( pow2( s0 ) );
 	const double t3_x = magic5 * v1_x - magic5 * v2_x;
 	const double t3_y = magic5 * v1_y - magic5 * v2_y;
 	const double t3_z = magic5 * v1_z - magic5 * v2_z;
 	const double t4_x = v1_x + v2_x;
 	const double t4_y = v1_y + v2_y;
 	const double t4_z = v1_z + v2_z;
-	const double helper_46 = ( 0.666666666666667 * 0.5 ) *
-							 ( v0_y * ( -3 * v0_y + v3_y + t4_y ) + v1_z * ( -3 * v1_z + v2_z + v3_z + v0_z ) + v2_z * ( v1_z - 3 * v2_z + v3_z + v0_z ) +
-							   v3_z * ( -3 * v3_z + t4_z + v0_z ) + v3_y * ( v0_y - 3 * v3_y + t4_y ) + v2_x * ( -3 * v2_x + v0_x + v1_x + v3_x ) +
-							   v1_y * ( v0_y + v3_y - 3 * v1_y + v2_y ) + v2_y * ( v0_y + v3_y + v1_y - 3 * v2_y ) + v0_x * ( -3 * v0_x + t4_x + v3_x ) +
-							   v1_x * ( v2_x + v0_x - 3 * v1_x + v3_x ) + v3_x * ( v0_x + t4_x - 3 * v3_x ) + v0_z * ( v3_z + t4_z - 3 * v0_z ) ) /
-							 helper_39;
-	result_0[ 0 ] = helper_40 * ( -v2_x + 3.0 * v0_x - helper_46 * ( -t3_z * t0_y + t3_y * t0_z - t1_z * t2_y + t1_y * t2_z ) - v1_x - v3_x );
-	result_0[ 1 ] = helper_40 * ( 3.0 * v0_y - v3_y - v1_y - v2_y + helper_46 * ( helper_23 + t0_z * t3_x - t0_x * t3_z ) );
-	result_0[ 2 ] = helper_40 * ( -v1_z - v2_z - v3_z + helper_46 * ( -t0_y * t3_x - helper_32 + helper_37 + t0_x * t3_y ) + 3.0 * v0_z );
+	const double s2 = ( 0.666666666666667 * 0.5 ) *
+					  ( v0_y * ( -3 * v0_y + v3_y + t4_y ) + v1_z * ( -3 * v1_z + v2_z + v3_z + v0_z ) + v2_z * ( v1_z - 3 * v2_z + v3_z + v0_z ) +
+						v3_z * ( -3 * v3_z + t4_z + v0_z ) + v3_y * ( v0_y - 3 * v3_y + t4_y ) + v2_x * ( -3 * v2_x + v0_x + v1_x + v3_x ) +
+						v1_y * ( v0_y + v3_y - 3 * v1_y + v2_y ) + v2_y * ( v0_y + v3_y + v1_y - 3 * v2_y ) + v0_x * ( -3 * v0_x + t4_x + v3_x ) +
+						v1_x * ( v2_x + v0_x - 3 * v1_x + v3_x ) + v3_x * ( v0_x + t4_x - 3 * v3_x ) + v0_z * ( v3_z + t4_z - 3 * v0_z ) ) /
+					  s0;
+	result_0[ 0 ] = s1 * ( -v2_x + 3.0 * v0_x - s2 * ( -t3_z * t0_y + t3_y * t0_z - t1_z * t2_y + t1_y * t2_z ) - v1_x - v3_x );
+	result_0[ 1 ] = s1 * ( 3.0 * v0_y - v3_y - v1_y - v2_y + s2 * ( helper_23 + t0_z * t3_x - t0_x * t3_z ) );
+	result_0[ 2 ] = s1 * ( -v1_z - v2_z - v3_z + s2 * ( -t0_y * t3_x - helper_32 + helper_37 + t0_x * t3_y ) + 3.0 * v0_z );
 }
 
 void floatTetWild::AMIPS_jacobian( const std::array<Scalar, 12>& T, Vector3& result_0 )
