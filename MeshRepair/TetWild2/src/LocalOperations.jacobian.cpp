b66779d2d7f6a7d56177cbcf841c7caf52ccd9d8
Konstantin <const@const.me>
2022-11-27 16:34:31 +0100

    Optimizing AMIPS Jacobian

--- MeshRepair/TetWild2/src/LocalOperations.jacobian.cpp
+++ MeshRepair/TetWild2/src/LocalOperations.jacobian.cpp
@@ -41,33 +41,39 @@ void floatTetWild::AMIPS_jacobian_v2( const std::array<Scalar, 12>& arr, Vector3
 	const double t0_y = v0_y - v3_y;
 	const double t0_z = v0_z - v3_z;
 
-	const double helper_7 = magic1 * v0_x - magic2 * v1_x + magic1 * v3_x;
-	const double helper_9 = magic3 * v0_z;
-	const double helper_11 = magic3 * v1_z;
-	const double helper_13 = magic4 * v2_z;
-	const double helper_15 = magic3 * v3_z;
-	const double helper_16 = helper_11 - helper_13 + helper_15 + helper_9;
+	const double t1_x = magic1 * v0_x - magic2 * v1_x + magic1 * v3_x;
+	const double helper_33 = magic1 * v0_y;
+	const double helper_34 = magic2 * v1_y;
+	const double helper_35 = magic1 * v3_y;
+	const double t1_y = helper_33 - helper_34 + helper_35;
+
 	const double helper_17 = magic1 * v0_z;
 	const double helper_18 = magic2 * v1_z;
 	const double helper_19 = magic1 * v3_z;
-	const double helper_20 = helper_17 - helper_18 + helper_19;
-	const double helper_22 = -magic4 * v2_x + magic3 * v0_x + magic3 * v1_x + magic3 * v3_x;
-	const double helper_23 = helper_16 * helper_7 - helper_20 * helper_22;
+	const double t1_z = helper_17 - helper_18 + helper_19;
+
+	const double t2_x = magic3 * v0_x - magic4 * v2_x + magic3 * v1_x + magic3 * v3_x;
+
 	const double helper_25 = magic3 * v0_y;
 	const double helper_27 = magic3 * v1_y;
 	const double helper_29 = magic4 * v2_y;
 	const double helper_30 = magic3 * v3_y;
-	const double helper_31 = helper_25 + helper_27 - helper_29 + helper_30;
-	const double helper_32 = helper_31 * helper_7;
-	const double helper_33 = magic1 * v0_y;
-	const double helper_34 = magic2 * v1_y;
-	const double helper_35 = magic1 * v3_y;
-	const double helper_36 = helper_33 - helper_34 + helper_35;
-	const double helper_37 = helper_22 * helper_36;
-	const double helper_39 = helper_23 * t0_y - t0_z * ( helper_32 - helper_37 ) - t0_x * ( helper_16 * helper_36 - helper_20 * helper_31 );
+	const double t2_y = helper_25 + helper_27 - helper_29 + helper_30;
+
+	const double helper_9 = magic3 * v0_z;
+	const double helper_11 = magic3 * v1_z;
+	const double helper_13 = magic4 * v2_z;
+	const double helper_15 = magic3 * v3_z;
+	const double t2_z = helper_11 - helper_13 + helper_15 + helper_9;
+
+	const double helper_23 = t2_z * t1_x - t1_z * t2_x;
+	const double helper_32 = t2_y * t1_x;
+	const double helper_37 = t2_x * t1_y;
+	const double helper_39 = helper_23 * t0_y - t0_z * ( helper_32 - helper_37 ) - t0_x * ( t2_z * t1_y - t1_z * t2_y );
 	const double helper_40 = 1.0 / cubicRoot( pow2( helper_39 ) );
-	const double helper_41 = magic5 * v1_z - magic5 * v2_z;
+	const double helper_47 = magic5 * v1_x - magic5 * v2_x;
 	const double helper_42 = magic5 * v1_y - magic5 * v2_y;
+	const double helper_41 = magic5 * v1_z - magic5 * v2_z;
 	const double helper_43 = v1_x + v2_x;
 	const double helper_44 = v1_y + v2_y;
 	const double helper_45 = v1_z + v2_z;
@@ -77,7 +83,6 @@ void floatTetWild::AMIPS_jacobian_v2( const std::array<Scalar, 12>& arr, Vector3
 							   v1_y * ( v0_y + v3_y - 3 * v1_y + v2_y ) + v2_y * ( v0_y + v3_y + v1_y - 3 * v2_y ) + v0_x * ( -3 * v0_x + helper_43 + v3_x ) +
 							   v1_x * ( v2_x + v0_x - 3 * v1_x + v3_x ) + v3_x * ( v0_x + helper_43 - 3 * v3_x ) + v0_z * ( v3_z + helper_45 - 3 * v0_z ) ) /
 							 helper_39;
-	const double helper_47 = -magic5 * v2_x + magic5 * v1_x;
 	result_0[ 0 ] = -helper_40 * ( v2_x - 3.0 * v0_x +
 								   helper_46 * ( helper_41 * ( -v0_y + v3_y ) - helper_42 * ( v3_z - v0_z ) -
 												 ( -helper_17 + helper_18 - helper_19 ) * ( -helper_25 - helper_27 + helper_29 - helper_30 ) +
