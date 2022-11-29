de3d8fb3c9db0ef99bbbb9b67ca926d90fc2c96e
Konstantin <const@const.me>
2022-11-29 21:52:41 +0100

    Minor

--- MeshRepair/MeshRepair/API/loggerApi.h
+++ MeshRepair/MeshRepair/API/loggerApi.h
@@ -15,12 +15,13 @@ namespace MeshRepair
 	// C function pointer to receive log messages from the library. The messages are encoded in UTF-8.
 	using pfnLoggerSink = void( __cdecl* )( void* context, eLogLevel lvl, const char* message );
 
+	// A sink to receive log messages produced by MeshRepair.dll
 	struct sLoggerSetup
 	{
 		// C function pointer to receive log messages from the library
-		pfnLoggerSink sink;
+		pfnLoggerSink sink = nullptr;
 		// Optional context parameter for the sink function; when consuming from C# you don't need that, pass IntPtr.Zero, delegates can capture things.
-		void* context;
+		void* context = nullptr;
 		// Maximum log level to produce
 		eLogLevel level;
 	};
